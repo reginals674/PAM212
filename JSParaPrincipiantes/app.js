5c5938150ce483a36986da1e0a44a399f6d5b13d
reginals674 <lopregi08@gmail.com>
2025-09-09 18:24:35 -0600

    como tipos de dato se encuentra String que puede ser con comillas dobles o simples.

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -3,5 +3,7 @@ document.write ("<h1>Hello world</h1>");
  console.error ("mi error customatizado")
  console.error("La aplicación ha fallado")
 
+  //tipos de datos
+  "hello world"//string 
+  'hello world'//string
 
-  
