3352367876af6e5b9b5e938ff58975afca1455f9
reginals674 <lopregi08@gmail.com>
2025-09-11 07:57:14 -0600

    Ejemplificamos el uso de comentarios para documentar el código

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,9 +1,17 @@
+// creacion de variables
  var nameuser = "John";
  let lastname ="caracter";
 
+//reasignacion del valor de la variable nameuser
  nameuser ='pepe';
  console.log(nameuser)
- 
+
+//creacion de una constante y el no poder reasignar valores
 const PI=3.1415
 PI= 100;
 console.log(PI)
+
+/*otra 
+variable 
+definida*/
+let userName = 'gordon';
