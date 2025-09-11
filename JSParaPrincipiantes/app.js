f4fe5f1fb5bf171f12cceb3779922da15608222c
reginals674 <lopregi08@gmail.com>
2025-09-11 09:53:38 -0600

    Por ultimo se menciona el uso de funciones tanto sin parámetros como con parámetros y con diferentes tipos de dato

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,24 +1,35 @@
-//bucles
+//funciones 
 
-
-let count =50;
-while (count >0){
-    console.log(count);
-    count--;
+function greeting (){
+    console.log("Hello");
 }
 
+greeting(); //llamada a la funcion  
 
-count =1 ;
-while (count <=50){
-    console.log(count);
-    count++;
-}   
+greeting(); 
+greeting(); 
+greeting(); 
+greeting(); 
 
-let names=['ryan','joe','john'];
-for (let i = 0; i > names.length; i++){
-    console.log(names[i]);
-}   
 
-for (let i = names.lengt - 1 ; i >= 0; i--){
-    console.log(names[i]);
+function greeting (name){
+    console.log(name)
+    console.log("Hello");
+}
+greeting("Markus");
+
+function greeting (personName){
+    console.log('hello'+personName);
+}
+greeting("Kara");
+
+greeting("Markus");
+
+greeting("Connor");
+
+function add (num1,num2){
+    console.log(num1+num2);
 }   
+add(3,2);
+add(100,300);
+add(5000,2000);
