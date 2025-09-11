5ad684e4de241e4d2168e7feeed44db7fa3066ab
reginals674 <lopregi08@gmail.com>
2025-09-11 09:46:40 -0600

    se hace uso de bucles para recorrer un arreglo y llevar una cuenta

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,12 +1,24 @@
-let typeCard= 'Credit card';
-switch(typeCard){
-    case 'Debit card':
-        console.log('this is a debit card');
-        break;
-    case 'Credit card':
-        console.log('this is a credit card'); 
-        break;
-        default:
-        console.log('No card');
-        break;
+//bucles
+
+
+let count =50;
+while (count >0){
+    console.log(count);
+    count--;
 }
+
+
+count =1 ;
+while (count <=50){
+    console.log(count);
+    count++;
+}   
+
+let names=['ryan','joe','john'];
+for (let i = 0; i > names.length; i++){
+    console.log(names[i]);
+}   
+
+for (let i = names.lengt - 1 ; i >= 0; i--){
+    console.log(names[i]);
+}   
