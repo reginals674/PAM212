6744074a27d09b14af7c75d8b6f89d3f1a394ec1
reginals674 <lopregi08@gmail.com>
2025-09-18 18:54:18 -0600

    incluimos cambio a let

--- Practica 1/variables.js
+++ Practica 1/variables.js
@@ -3,6 +3,6 @@ let edad = 25;
 
 nombre = "Ana Maria";
 
-var saludo = "Hola, "+ nombre +". Tienes "+ edad +" años.";
+let saludo = "Hola, "+ nombre +". Tienes "+ edad +" años.";
 
 console.log(saludo);
