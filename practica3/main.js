f20bb5a9e382c4def338b311623249b456fca1bf
reginals674 <lopregi08@gmail.com>
2025-09-25 19:18:02 -0600

    Se crea una función obtenerDatos() que simule una llamada a una API con setTimeout y usar async/await

--- practica3/main.js
+++ practica3/main.js
@@ -15,3 +15,28 @@ verificarUsuario("admin")
 verificarUsuario("Regina")
     .then(res => console.log(res))
     .catch(err => console.log(err));    
+
+
+
+//Crea una función obtenerDatos() que simule una llamada a una API con setTimeout y usar async/await para esperar el resultado.
+
+function simularPeicionAPI() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Datos obtenidos de la API");
+        }, 5000);
+    });
+}
+
+async function obtenerDatos() {
+    const resultado = await simularPeicionAPI();
+    console.log(resultado);
+    return resultado;
+}   
+
+obtenerDatos()
+    .then(datos => {console.log("Proceso completado: ", datos);})
+    .catch(err => {console.log("Error:", err);
+    });
+
+//Crea una función que use fetch para obtener datos de una API pública y maneje los errores con try/catch.});
