4d9570ad13226caedf4a3dbc4e30a04dda9e46a5
reginals674 <lopregi08@gmail.com>
2025-10-09 18:45:08 -0600

    se hace uso de propiedades y de hook para cambiar un contador mediante botones

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,25 @@
+//import: Zona de declaraciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import React, { useState } from 'react';
 
+//2. zona main/principal se levantan compnentes
 export default function App() {
+  const [contador, setContador]=useState(0);
   return (
     <View style={styles.container}>
-      <Text>Hola mundo React Native </Text>
+
+      <Text>Contador: {contador}</Text>
+      <Button title="Incrementar" onPress={()=>setContador(contador+1)}/>
+      <Button title="Disminuir" onPress={()=>setContador(contador-1)}/>
+      <Button title="Iniciar" onPress={()=>setContador(0)}/>
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
-
+//3.zona de estilos:estetica y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
