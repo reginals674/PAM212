d857bb97d193c94ff4c9d4dc5d42bf0125b586c4
reginals674 <lopregi08@gmail.com>
2025-11-07 18:10:27 -0600

    primer commit examen

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Switch,useState } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hola</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const [isEnabled, setIsEnabled] = useState(false);
+const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+
+const Header = () => {
+    return(
+      <View style = {styles.header}>
+        <Text>Articulos</Text>
+      </View>
+    );
+};
+
+export default function scrollArticulos(){
+  return(
+      <View >
+        <Header/>
+        <ScrollView>
+         
+
+          <View style = {styles.Tarjeta}>
+            <Text>NOMBRE ART.</Text>
+            <Text>fecha</Text>
+            <Text>resumen</Text>
+            <Switch  
+              trackColor={{ false: "#757775ff", true: "#74a1c5ff" }}
+                  thumbColor={isEnabled ? "#97ca97ff" : "#f4f3f4ff"}
+                
+                onValueChange={toggleSwitch}
+                value={isEnabled}
+            />
+          </View>
+
+        </ScrollView>
+      </View>
+    );
+
+};
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+
+  header:{
+    
+    height: 120,
+    backgroundColor: '#181D31',
     justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 25,
+
   },
+   Tarjeta:{
+    
+    backgroundColor: '#6d82d7ff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 25,
+
+   }
+
+
 });
