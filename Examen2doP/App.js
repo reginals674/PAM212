23006092e11ef933b58046f05df8417ca1496b88
reginals674 <lopregi08@gmail.com>
2025-11-07 18:50:38 -0600

    ultimo commit

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,36 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Switch,useState } from 'react-native';
 
-const [isEnabled, setIsEnabled] = useState(false);
-const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+import React, {useEffect, useRef,useState}from 'react';
+import { StyleSheet, Text, View, ScrollView, Switch,Alert} from 'react-native';
+import { Button } from 'react-native-web';
 
 
-const Header = () => {
-    return(
-      <View style = {styles.header}>
-        <Text>Articulos</Text>
-      </View>
-    );
-};
 
 export default function scrollArticulos(){
+
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => { setIsEnabled(previousState => !previousState)
+
+      Alert.alert('atencion', '');
+      alert('Error: Por favor complete todos los campos, falta nombre',
+        [
+          
+        ]
+      );
+    };
+
+   
   return(
       <View >
-        <Header/>
+        <View style = {styles.header}>
+        <Text style= {styles.text}>Blog de Noticias</Text>
+      </View>
         <ScrollView>
          
+          <Text style ={styles.tittle}>TECNOLOGÍA</Text>
 
           <View style = {styles.Tarjeta}>
-            <Text>NOMBRE ART.</Text>
-            <Text>fecha</Text>
-            <Text>resumen</Text>
-            <Switch  
-              trackColor={{ false: "#757775ff", true: "#74a1c5ff" }}
-                  thumbColor={isEnabled ? "#97ca97ff" : "#f4f3f4ff"}
-                
-                onValueChange={toggleSwitch}
-                value={isEnabled}
-            />
+            
+            <Text>World of Warcraft anuncia una moneda virtual para comprar artículos con los que decorar los hogares</Text>
+            <Text>10/08/25</Text>
+            <Text >Blizzard ha anunciado la introducción de una nueva moneda virtual en World of Warcraft,
+            Hearthsteel es el nombre de la moneda virtual que podrá adquirirse con dinero real y que permitirá a los jugadores de World of Warcraft comprar elementos decorativos para personalizar los hogares.</Text>
+
+            <Button  title="leer mas.."  />
+            
+          </View>
+           <View style = {styles.Tarjeta}>
+            
+            <Text>World of Warcraft anuncia una moneda virtual para comprar artículos con los que decorar los hogares</Text>
+            <Text>10/08/25</Text>
+            <Text>Blizzard ha anunciado la introducción de una nueva moneda virtual en World of Warcraft,
+            Hearthsteel es el nombre de la moneda virtual que podrá adquirirse con dinero real y que permitirá a los jugadores de World of Warcraft comprar elementos decorativos para personalizar los hogares.</Text>
+
+           <Switch
+            trackColor={{ false: "#757775ff", true: "#74a1c5ff" }}
+            thumbColor={isEnabled ? "#97ca97ff" : "#f4f3f4ff"}
+            
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+            
+          />
+            
           </View>
 
         </ScrollView>
@@ -56,9 +79,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#6d82d7ff',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingTop: 25,
+    marginBlock: 40,
+    paddingLeft:55,
+    paddingRight:55,
+    paddingBottom:55,
+    paddingEnd:55,
 
-   }
+   },
+  text:{
+    color: "#fff",
+    fontSize: 24,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+
+  tittle:{
+    color: "#000",
+    fontSize: 24,
+    marginBottom: 10,
+    textAlign: "center",
+  },
 
 
 });
